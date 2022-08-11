149ecb8778bc16391f59d846a73a9d5566954cdd
Vergel Mañalac <verge2015@gmail.com>
2022-08-11 17:08:40 +0800

    Remove default CSS files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Parent from './components/Parent';
 
 function App() {
   return (
-    <>
+    <div className='App-Header'>
     <center>
       <h1>Pass Value from Child to Parent using Callback</h1>
       <Parent/>
     </center>
-    </>
+    </div>
   );
 }
 
